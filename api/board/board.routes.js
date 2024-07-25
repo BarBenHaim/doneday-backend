54080555f9803b5917999535ecd08f3b1af6e2cb
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-07-25 14:43:57 +0300

    Init doneday backend

---
+++ api/board/board.routes.js
@@ -0,0 +1,23 @@
+import express from 'express'
+
+import { requireAuth } from '../../middlewares/requireAuth.middleware.js'
+import { log } from '../../middlewares/logger.middleware.js'
+
+import { getBoards, getBoardById, addBoard, updateBoard, removeBoard, addBoardMsg, removeBoardMsg } from './board.controller.js'
+
+const router = express.Router()
+
+// We can add a middleware for the entire router:
+// router.use(requireAuth)
+
+router.get('/', log, getBoards)
+router.get('/:id', log, getBoardById)
+router.post('/', log, addBoard)
+router.put('/:id', updateBoard)
+router.delete('/:id', removeBoard)
+// router.delete('/:id', requireAuth, requireAdmin, removeBoard)
+
+router.post('/:id/msg', addBoardMsg)
+router.delete('/:id/msg/:msgId', removeBoardMsg)
+
+export const boardRoutes = router
