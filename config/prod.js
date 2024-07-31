cdd2dc32342a64e06df1890157e2370456321b3a
Bar Ben Haim <barbenbh@gmail.com>
2024-07-31 14:05:20 +0300

    fix connecting db

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,7 @@
 export default {
-    dbURL: process.env.MONGO_URL || 'mongodb+srv://barbenbh:96352414@doneday.taxyz0e.mongodb.net/',
+    dbURL:
+        process.env.MONGO_URL ||
+        'mongodb+srv://barbenbh:96352414@doneday.taxyz0e.mongodb.net/?retryWrites=true&w=majority&appName=doneday',
+
     dbName: process.env.DB_NAME || 'doneday',
 }
