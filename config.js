86d113f67e732cbe2d3ec3be35e29355506d278a
Bar Ben Haim <barbenbh@gmail.com>
2024-08-02 11:15:58 +0300

    fix api

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 import dotenv from 'dotenv'
-import { OPENAI_API_KEY } from './config/openai_key'
 dotenv.config()
-export const API_KEY = OPENAI_API_KEY
+export const API_KEY = process.env.OPENAI_API_KEY
