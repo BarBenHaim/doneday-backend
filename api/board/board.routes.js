d36c8f1841175b92163f3b95d9406c7ce0d65a2f
Ariella Melnikov <melnikov.ariella@gmail.com>
2024-08-13 09:10:59 +0300

    delete authReqier

--- api/board/board.routes.js
+++ api/board/board.routes.js
@@ -23,27 +23,50 @@ import {
     getBoardActivities,
 } from './board.controller.js'
 
-const router = express.Router()
+// const router = express.Router()
+// router.get('/', log, getBoards)
+// router.get('/initDB', initDB)
+// router.get('/:boardId', log, requireAuth, getBoardById)
+// router.post('/', log, requireAuth, addBoard)
+// router.put('/:boardId', log, requireAuth, updateBoard)
+// router.delete('/:boardId', log, requireAuth, removeBoard)
+
+// router.post('/:boardId/group', log, requireAuth, addGroup)
+// router.put('/:boardId/:groupId', log, requireAuth, updateGroup)
+// router.delete('/:boardId/:groupId', log, requireAuth, removeGroup)
+
+// router.post('/:boardId/:groupId/task', log, requireAuth, addTask)
+// router.put('/:boardId/:groupId/:taskId', log, requireAuth, updateTask)
+// router.delete('/:boardId/:groupId/:taskId', log, requireAuth, removeTask)
+
+// router.post('/:boardId/:groupId/:taskId/comment', log, requireAuth, addComment)
+// router.put('/:boardId/:groupId/:taskId/:commentId', log, requireAuth, updateComment)
+// router.delete('/:boardId/:groupId/:taskId/:commentId', log, requireAuth, deleteComment)
+// router.get('/:boardId/:groupId/:taskId/comment', log, requireAuth, getComments)
+
+// router.get('/:boardId/activities', log, requireAuth, getBoardActivities)
+
 router.get('/', log, getBoards)
 router.get('/initDB', initDB)
-router.get('/:boardId', log, requireAuth, getBoardById)
-router.post('/', log, requireAuth, addBoard)
-router.put('/:boardId', log, requireAuth, updateBoard)
-router.delete('/:boardId', log, requireAuth, removeBoard)
+router.get('/:boardId', log, getBoardById)
+router.post('/', log, addBoard)
+router.put('/:boardId', log, updateBoard)
+router.delete('/:boardId', log, removeBoard)
 
-router.post('/:boardId/group', log, requireAuth, addGroup)
-router.put('/:boardId/:groupId', log, requireAuth, updateGroup)
-router.delete('/:boardId/:groupId', log, requireAuth, removeGroup)
+router.post('/:boardId/group', log, addGroup)
+router.put('/:boardId/:groupId', log, updateGroup)
+router.delete('/:boardId/:groupId', log, removeGroup)
 
-router.post('/:boardId/:groupId/task', log, requireAuth, addTask)
-router.put('/:boardId/:groupId/:taskId', log, requireAuth, updateTask)
-router.delete('/:boardId/:groupId/:taskId', log, requireAuth, removeTask)
+router.post('/:boardId/:groupId/task', log, addTask)
+router.put('/:boardId/:groupId/:taskId', log, updateTask)
+router.delete('/:boardId/:groupId/:taskId', log, removeTask)
 
-router.post('/:boardId/:groupId/:taskId/comment', log, requireAuth, addComment)
-router.put('/:boardId/:groupId/:taskId/:commentId', log, requireAuth, updateComment)
-router.delete('/:boardId/:groupId/:taskId/:commentId', log, requireAuth, deleteComment)
-router.get('/:boardId/:groupId/:taskId/comment', log, requireAuth, getComments)
+router.post('/:boardId/:groupId/:taskId/comment', log, addComment)
+router.put('/:boardId/:groupId/:taskId/:commentId', log, updateComment)
+router.delete('/:boardId/:groupId/:taskId/:commentId', log, deleteComment)
+router.get('/:boardId/:groupId/:taskId/comment', log, getComments)
 
 router.get('/:boardId/activities', log, requireAuth, getBoardActivities)
+
 
 export const boardRoutes = router
